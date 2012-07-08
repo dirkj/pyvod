1e51769c777d271bdb060f03e48937e8c36e8dcd
Dirk Jahnke <dirk@familie-jahnke.de>
2012-07-08 14:19:48 +0200

    Fixed syntax error when loading schema definition

--- Controller/schemaController.py
+++ Controller/schemaController.py
@@ -19,9 +19,9 @@ class SchemaController:
 			if dE.tag == "line":
 				self.view.addLine(x1=dE.attrib["x1"], y1=dE.attrib["y1"], x2=dE.attrib["x2"], y2=dE.attrib["y2"], color=dE.attrib["color"], width=2, track=None if not "track" in dE.attrib else dE.attrib["track"])  
 			elif dE.tag == "rectangle":
-				self.view.addRectangle(x1=dE.attrib["x1"], y1=dE.attrib["y1"], x2=dE.attrib["x2"], y2=dE.attrib["y2"], color=dE.attrib["color"], width=2), track=None if not "track" in dE.attrib else dE.attrib["track"])  
+				self.view.addRectangle(x1=dE.attrib["x1"], y1=dE.attrib["y1"], x2=dE.attrib["x2"], y2=dE.attrib["y2"], color=dE.attrib["color"], width=2, track=None if not "track" in dE.attrib else dE.attrib["track"])  
 			elif dE.tag == "circle":
-				self.view.addCircle(x1=dE.attrib["x1"], y1=dE.attrib["y1"], r=dE.attrib["r"], color=dE.attrib["color"]), track=None if not "track" in dE.attrib else dE.attrib["track"])  
+				self.view.addCircle(x1=dE.attrib["x1"], y1=dE.attrib["y1"], r=dE.attrib["r"], color=dE.attrib["color"], track=None if not "track" in dE.attrib else dE.attrib["track"])  
 
 
 	def setView(self, view):
